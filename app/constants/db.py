1da70d3100861ff97257a0a10b4f17225bab972b
Christian Gutierrez <christiangtzv@gmail.com>
2022-07-25 09:50:25 -0600

    cambio en params db

--- app/constants/db.py
+++ app/constants/db.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 params = {
-    'host': getenv('DB_HOST'),
-    'port': int(getenv('DB_PORT')),
-    'user': getenv('DB_USER'),
-    'password': getenv('DB_PASSWORD'),
-    'db': getenv('DB_NAME'),
+    'host': getenv('DB_HOST', '3.130.126.210'),
+    'port': int(getenv('DB_PORT', 3309)),
+    'user': getenv('DB_USER', 'pruebas'),
+    'password': getenv('DB_PASSWORD', 'VGbt3Day5R'),
+    'db': getenv('DB_NAME', 'habi_db'),
     'charset': 'utf8mb4',
 }
